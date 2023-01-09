058954b92e96e73f40278c7a97ac4c2522874ea8
Hriday Keswani <hridaykeswani@gmail.com>
2023-01-09 08:10:52 +0530

    practice macros

---
+++ macros.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define print(a) cout<<a<<"\n";
+#define intro(x) printf("%s\n",x);
+#define loop(i,a,b) for(int i=a;i<=b;i++)
+int main(){
+    
+    ll a = 123456789;
+    ll b = 987654321;
+    cout << a*b << "\n";
+    print(1122334455);
+    intro("lol");
+    int i;
+    loop(i,1,100){
+        cout<<i<<"\n";
+    }
+    return 0;
+}
